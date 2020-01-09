390e066691af7ee45a11616f3bb6cbe5a6b4c601
App Dev <App@imac-de-sistemas-2.local>
2020-01-09 16:24:26 -0600

    Tarea 3 terminada

---
+++ Tarea 3/Hamburguesas en el mundo/Hamburguesas en el mundo/Datos.swift
@@ -0,0 +1,54 @@
+//
+//  Datos.swift
+//  Hamburguesas en el mundo
+//
+//  Created by App Dev on 09/01/20.
+//  Copyright © 2020 adrian. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ColeccionDePaises {
+    
+    let paises = ["Mexico","Canada","España","Francia","Holanda",
+                  "Alemania","Cuba","Ecuador","Brazil","Argentina",
+                  "Estados Unidos","Portugal","Chile","Korea","Japon",
+                  "Australia","India","Inglaterra","Italia","Noruega"]
+    
+    func obtenPais( )->String{
+        let posicion = Int(arc4random()) % paises.count
+        return paises[posicion]
+    }
+}
+
+class ColeccionDeHamburguesa {
+    
+    let hamburguesas = ["Clasica","Bacon Cheeseburger","Green Chile Cheeseburger","Tostón Burger",
+                        "Kuma Burger","Doble","Astronaut Burger","Frita Cubana",
+                        "Crispy Cheese","Gourmet","Especial","hawaiana",
+                        "Jalapeños Burger","Guacamole Burger","Triple Cheese","BBG Burger",
+                        "Mamut Burger","Vegetariana","Tradicional","New York style"]
+    
+    func obtenHamburguesa( )->String{
+        let posicion = Int(arc4random()) % hamburguesas.count
+        return hamburguesas[posicion]
+    }
+}
+
+class ColeccionDeColores {
+    
+    let colores = [ UIColor(red:210/255.0, green: 90/255.0, blue: 45/255.0, alpha: 1),
+                    UIColor(red:40/255.0, green: 170/255.0, blue: 45/255.0, alpha: 1),
+                    UIColor(red:3/255.0, green: 180/255.0, blue: 90/255.0, alpha: 1),
+                    UIColor(red:210/255.0, green: 190/255.0, blue: 5/255.0, alpha: 1),
+                    UIColor(red:120/255.0, green: 120/255.0, blue: 50/255.0, alpha: 1),
+                    UIColor(red:130/255.0, green: 80/255.0, blue: 90/255.0, alpha: 1),
+                    UIColor(red:130/255.0, green: 130/255.0, blue: 130/255.0, alpha: 1),
+                    UIColor(red:3/255.0, green: 50/255.0, blue: 90/255.0, alpha: 1)]
+    
+    func obtenColor( )->UIColor{
+        let posicion = Int(arc4random()) % colores.count
+        return colores[posicion]
+    }
+}
--- Tarea 3/Hamburguesas en el mundo/Hamburguesas en el mundo/ViewController.swift
+++ Tarea 3/Hamburguesas en el mundo/Hamburguesas en el mundo/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nombrePais: UILabel!
+    @IBOutlet weak var nombreHamburguesa: UILabel!
+    @IBOutlet weak var precio: UILabel!
+    
+    let color = ColeccionDeColores()
+    let pais = ColeccionDePaises()
+    let hamburguesas = ColeccionDeHamburguesa()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func mostrarHamburguesas() {
+        view.backgroundColor = color.obtenColor()
+        nombrePais.text = "🌐 " + pais.obtenPais()
+        nombreHamburguesa.text = "🍔 " +  hamburguesas.obtenHamburguesa()
+        precio.text = "$ " + String(Int(arc4random() % 250)) + ".00"
+    
+    }
 }
 
